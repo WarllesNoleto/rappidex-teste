3e81634c5ee4d18f4c761576af5b7c4d57f5c6e2
Warlles Noleto <142702004+WarllesNoleto@users.noreply.github.com>
2026-05-25 13:08:40 -0300

    a

---
+++ rappidexexpress-front-teste/src/shared/interfaces/user.interface.ts
@@ -0,0 +1,18 @@
+export interface User {
+  id: string;
+  isActive: boolean;
+  ifoodMerchantId?: string;
+  ifoodClientId?: string;
+  location: string;
+  name: string;
+  permission: string;
+  phone: string;
+  profileImage: string;
+  type: string;
+  user: string;
+  useIfoodIntegration?: boolean;
+  usesExternalIfoodPdv?: boolean;
+  ifoodOrdersReleased?: number;
+  ifoodOrdersUsed?: number;
+  ifoodOrdersAvailable?: number;
+}
